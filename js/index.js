3df593e1a80a405fb9bf93cbed142fdf9c1fe58a
inottn <inottn@outlook.com>
2018-11-14 10:32:14 +0800

    add: 添加了radio-icon元素的点击事件，使其替代HTML原生radio元素

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
 const cloudbgWrapper = document.querySelector('.cloudbg-wrapper')
 const bookNowBtn = document.querySelector('.book-now-btn')
+const radios = document.querySelectorAll('.radio-group input')
+const radioIcons = document.querySelectorAll('.radio-group .radio-icon')
+const radioLabels = document.querySelectorAll('.radio-group label')
 
 bookNowBtn.addEventListener('click', function(e) {
   cloudbgWrapper.classList.add('active')
+})
+
+Array.from(radioIcons).forEach(function(icon, index, icons) {
+  icon.addEventListener('click', function(e) {
+    this.classList.add('active')
+    radios[index].click()
+    icons.forEach((icon) => {
+      if (icon === this) return
+      icon.classList.remove('active')
+    })
+  })
+})
+
+radioLabels.forEach(function(label, index, labels) {
+  label.addEventListener('click', function(e) {
+    radioIcons[index].click()
+    radios[index].click()
+  })
 })
