5c70d9f06b515829a19ccf4f2d46743062e30d95
inottn <inottn@outlook.com>
2018-11-15 08:15:24 +0800

    add: 完成了return-btn元素的基本布局和过渡样式并为其添加了点击事件

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 const cloudbgWrapper = document.querySelector('.cloudbg-wrapper')
 const bookNowBtn = document.querySelector('.book-now-btn')
+const returnBtn = document.querySelector('.return-btn')
 const radios = document.querySelectorAll('.radio-group input')
 const radioIcons = document.querySelectorAll('.radio-group .radio-icon')
 const radioLabels = document.querySelectorAll('.radio-group label')
 
 bookNowBtn.addEventListener('click', function(e) {
   cloudbgWrapper.classList.add('active')
+})
+
+returnBtn.addEventListener('click', function(e) {
+  cloudbgWrapper.classList.remove('active')
 })
 
 Array.from(radioIcons).forEach(function(icon, index, icons) {
